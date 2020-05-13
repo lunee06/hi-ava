925553f9c28483cf897da338976318a71a0cece7
lunee06 <65259305+lunee06@users.noreply.github.com>
2020-05-13 09:51:28 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
         class="vue-typed"
         :showCursor="false"
         :loop="true"
-        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'I always wanted to tell you... ^2500', 'Nevermind! >///<']"
+        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'لا تذهب الحبتي ^2500', 'سأكون دائما هنا من أجلك']"
         :backDelay="500"
         :typeSpeed="100"
         :backSpeed="20"
